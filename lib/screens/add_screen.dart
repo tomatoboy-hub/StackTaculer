d4d644518fe2f22029f388c8c8334034804d4684
ketyia1111 <work.keita1111@gmail.com>
2024-02-09 11:48:22 +0900

    addscanfunction_0209

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
+import './scan_screen.dart';
 
 class AddScreen extends StatelessWidget {
   final _controller = TextEditingController();
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add New Book')
+        title: Text('Add New Book'),
       ),
       body: Center(
         child: Padding(
-          padding:EdgeInsets.all(8.0),
+          padding: EdgeInsets.all(8.0),
           child: Column(
             children: <Widget>[
               TextField(
                 controller: _controller,
                 decoration: InputDecoration(
-                  labelText: 'Book Title'
+                  labelText: 'Book Title',
                 ),
               ),
               TextField(
                 decoration: InputDecoration(
-                  labelText: 'Author'
+                  labelText: 'Author',
                 ),
               ),
               ElevatedButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.pop(context, _controller.text);
                 },
                 child: Text('Add Book'),
-              )
-            ]
-          )
-        )
-      )
-
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt), // Changed from 'add' to 'camera_alt'
+        onPressed: () async {
+          final String? newBookTitle = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondScreen()),
+          );
+        },
+      ),
     );
-
   }
 }
